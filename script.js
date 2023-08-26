ba6693bf126fbe08f2bd78dc82dc5df69c96247c
Pramodya Fonseka <84384113+PramodyaFonseka@users.noreply.github.com>
2023-08-26 12:03:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close')
+const nav = document.getElementById('navbar');
+
+if (bar) {
+	bar.addEventListner('click', () => {
+		nav.classList.add('active');
+	})
+}
+
+if(close) {
+	close.addEventListner('click', () => {
+		nav.classList.remove('active');
+	})
+}
